15dc3247bd540c83b28b773082db0b54f62a1e1f
alfiyansaja <1alfiyanbahbah@gmail.com>
2013-11-16 20:31:30 +0700

    Create 2 variabel.rb
    contoh inisialisasi variabel

---
+++ introduction-ruby/2 variabel.rb
@@ -0,0 +1,18 @@
+### Variabel ###
+
+# variabel adalah sebuah tempat
+# untuk menyimpan sebuah data, contoh
+var_a = "var_a adalah variabel"
+puts var_a
+var_a = 123
+puts var_a
+var_a = 0.453
+puts var_a
+# penamaan sebuah variabel bersifat bebas
+# dari huruf kapital A-Z, a-z, awalan underscore(_)
+# jangan gunakan angka dan spesial character
+# sebagai nama variabel (0-9, !@#$%^, dll)
+Coba = "ini juga variabel"
+puts Coba
+_coba = 140
+puts _coba
