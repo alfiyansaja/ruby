b546f426161693fc389c6c8c23719f80f269601d
alfiyansaja <1alfiyanbahbah@gmail.com>
2013-11-16 20:30:27 +0700

    Create 1 intro_ruby.rb
    jangan lupa instal dulu ruby nya yaaa~

---
+++ introduction-ruby/1 intro_ruby.rb
@@ -0,0 +1,11 @@
+### PENGENALAN RUBY ###
+# program sederhana untuk menampilkan string
+print "Hello World!" # perintah menampilkan string dengan cursor berada dalam satu baris
+puts "Hello World!" # perintah menampilkan string dengan menambahkan baris baru
+
+# coba jalankan scriptnya dan temukan bedanya
+print "\n\n"
+# cara menggabungkan antara string satu dengan yang lain
+str = " World!"
+print "Hello" + str
+# maka yang tampil adalah Hello World!
